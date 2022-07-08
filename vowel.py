2b092dd64ed3d637f5446fbfdc9aae64b66af55c
chitu1629 <chitralekha.rajkumar@indiumsoft.com>
2022-07-08 12:55:41 +0530

    python commit

---
+++ vowel.py
@@ -0,0 +1,5 @@
+a = input('Enter a letter:')
+if ((a=='a') or (a=='e') or (a=='i') or (a=='o') or (a=='u') or (a=='A') or (a=='E') or (a=='I') or (a=='O') or (a=='U')):
+    print('Its a vowel!')
+else:
+    print('its a consonant!')
---
+++ words.py
@@ -0,0 +1,15 @@
+no = int(input("Enter:"))
+def num_in_words(no):
+    if (no == 1):
+        print("One")
+    elif (no == 2):
+        print("Two")
+    elif (no == 3):
+        print("Three")
+    else:
+        print("Give input of numbers from 1 to 3")
+
+
+num_in_words(no)
+
+#num_in_words(4.5)
